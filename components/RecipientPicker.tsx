cc41782afc78a3c35429f57f137a723d97c6244d
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-29 17:20:51 +0800

    Use `duix` for Recipient selection state.

--- components/RecipientPicker.tsx
+++ components/RecipientPicker.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Picker, PickerModes, PickerValue, View } from "react-native-ui-lib";
 import { InputLabel } from "./ui/InputLabel";
+import { Store } from "@/store";
 
 type Recipient = {
   id: string;
@@ -11,23 +12,33 @@ type Recipient = {
 const recipients: Recipient[] = [
   { id: "001", name: "user001", phoneNumber: "0171234561" },
   { id: "002", name: "user002", phoneNumber: "0171234562" },
-  { id: "001", name: "user003", phoneNumber: "0171234563" },
-  { id: "002", name: "user004", phoneNumber: "0171234564" },
-  { id: "001", name: "user005", phoneNumber: "0171234565" },
-  { id: "002", name: "user006", phoneNumber: "0171234566" },
+  { id: "003", name: "user003", phoneNumber: "0171234563" },
+  { id: "004", name: "user004", phoneNumber: "0171234564" },
+  { id: "005", name: "user005", phoneNumber: "0171234565" },
+  { id: "006", name: "user006", phoneNumber: "0171234566" },
 ];
 
 export function RecipientPicker() {
-  const [selectedAccount, setSelectedAccount] = useState<PickerValue>("");
+  const [selectedRecipient, setSelectedRecipient] = useState<PickerValue>("");
+
+  useEffect(() => {
+    const unsubscribe = Store.transfer.subscribeRecipient((newVal) => {
+      setSelectedRecipient(newVal);
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <View>
       <InputLabel label="To" />
       <Picker
         mode={PickerModes.SINGLE}
-        value={selectedAccount as any} // TODO -- Fix type
+        value={selectedRecipient as string} // TODO -- Fix type
         placeholder={"Select recipient"}
-        onChange={(selectedAcc) => setSelectedAccount(selectedAcc)}
+        onChange={(selectedRecipient) => {
+          Store.transfer.setRecipient(selectedRecipient as string);
+        }}
       >
         {recipients.map((acc) => (
           <Picker.Item label={acc.name} key={acc.id} value={acc.id} />
