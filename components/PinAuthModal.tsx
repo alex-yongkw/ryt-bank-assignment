5664b64d6bacfea606e3de02714cbc773db518c4
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-30 02:06:22 +0800

    Move pin verification into pin modal component.

--- components/PinAuthModal.tsx
+++ components/PinAuthModal.tsx
@@ -1,30 +1,56 @@
-import { Button, Modal, Text, View } from "react-native-ui-lib";
+import { Button, Text, View } from "react-native-ui-lib";
 import { StyleSheet } from "react-native";
-import { PinInput } from "@pakenfit/react-native-pin-input";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { PinInput, type PinInputRef } from "@pakenfit/react-native-pin-input";
 import { Colors } from "@/constants/Colors";
 import { Layout } from "@/constants/Layout";
 import { PopupModal } from "./ui/PopupModal";
+import { useCallback, useRef, useState } from "react";
+import { Secret } from "@/constants/Secret";
+import { InputError } from "./ui/InputError";
 
 type Props = {
   visible: boolean;
-  onPinFilled: (pin: string) => void;
+  onCorrectPin: () => void;
   onCancel: () => void;
 };
 
-export function PinAuthModal({ visible, onPinFilled, onCancel }: Props) {
+export function PinAuthModal({ visible, onCorrectPin, onCancel }: Props) {
+  const ref = useRef<PinInputRef>(null);
+  const [isIncorrectPin, setIsIncorrrectPin] = useState(false);
+
+  const verifyPin = useCallback(
+    (pin: string) => {
+      if (pin === Secret.appPin) {
+        onCorrectPin();
+      } else {
+        ref.current?.clear();
+        setIsIncorrrectPin(true);
+      }
+    },
+    [onCorrectPin, ref]
+  );
+
   return (
     <PopupModal visible={visible}>
       <Text text70BO style={styles.modalText}>
         Use app pin to authenticate
       </Text>
       <PinInput
+        ref={ref}
         length={6}
         autoFocus
-        onFillEnded={onPinFilled}
-        inputProps={{ secureTextEntry: true }}
+        onFillEnded={verifyPin}
+        inputProps={{
+          secureTextEntry: true,
+          onChange: () => setIsIncorrrectPin(false),
+        }}
         inputStyle={{ width: 50, height: 50 }}
       />
+      {isIncorrectPin && (
+        <View style={styles.errorLabel}>
+          <InputError label="Incorrect pin, please try again." />
+        </View>
+      )}
       <View style={styles.buttonContainer}>
         <Button
           label="Cancel"
@@ -42,6 +68,9 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 18,
     textAlign: "center",
+  },
+  errorLabel: {
+    marginTop: 8,
   },
   buttonContainer: {
     marginTop: 18,
