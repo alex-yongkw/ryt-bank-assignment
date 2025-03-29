2e9175dce59ef841953643a426c89e6b637acd1f
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-29 15:15:34 +0800

    Add <TransferAmount /> component.

---
+++ components/ui/TransferAmount.tsx
@@ -0,0 +1,31 @@
+import { Colors } from "@/constants/Colors";
+import { Text, View } from "react-native-ui-lib";
+import { useMemo } from "react";
+import { formatCurency } from "@/utils/number-formatter";
+import { TransferType } from "@/types";
+
+type Props = {
+  type: TransferType;
+  amount: number;
+};
+
+export function TransferAmount({ type, amount }: Props) {
+  const color = useMemo(
+    () => (type === "in" ? Colors.transferType.in : Colors.transferType.out),
+    [type]
+  );
+
+  const formattedAmount = useMemo(() => {
+    return type === "in"
+      ? `+ MYR ${formatCurency(amount)}`
+      : `- MYR ${formatCurency(amount)}`;
+  }, [type]);
+
+  return (
+    <View>
+      <Text text70BO color={color}>
+        {formattedAmount}
+      </Text>
+    </View>
+  );
+}
