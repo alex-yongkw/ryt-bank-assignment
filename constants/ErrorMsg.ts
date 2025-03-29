a9d2f88f86723fcfda2b3abe1a240d8fc1c1384a
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-29 18:48:14 +0800

    Add `ErrrorMsg` constant.

---
+++ constants/ErrorMsg.ts
@@ -0,0 +1,8 @@
+export const ErrorMsg = {
+  account: "Please select an account.",
+  recipient: "Please select a recipient.",
+  amount: {
+    invalid: "Please input a valid number.",
+    minimum: "The minimum transfer amount is MYR 1.00",
+  },
+};
