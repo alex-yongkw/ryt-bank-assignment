28daea7b02da7e6c831de2f541c7cef94f1566fe
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-30 11:34:46 +0800

    Get transaction history from singleton service.

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,10 +1,14 @@
-import { FlatList, StyleSheet } from "react-native";
-import { View } from "react-native-ui-lib";
-import { Layout } from "@/constants/Layout";
+import { TransactionHistoryCard } from "@/components/TransactionHistoryCard";
 import { SectionLabel } from "@/components/ui/SectionLabel";
 import { Colors } from "@/constants/Colors";
+import { Layout } from "@/constants/Layout";
+import { TransactionHistoryService } from "@/services/transaction-history";
 import { TransferType } from "@/types";
-import { TransactionHistoryCard } from "@/components/TransactionHistoryCard";
+import { formatCustomRelativeDate } from "@/utils/date-formatter";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
+import { FlatList, StyleSheet } from "react-native";
+import { View } from "react-native-ui-lib";
 
 type TransactionHistory = {
   id: string;
@@ -15,82 +19,37 @@ type TransactionHistory = {
   timestamp: string;
 };
 
-const transactionHistory: TransactionHistory[] = [
-  {
-    id: "001",
-    type: "in",
-    name: "user001",
-    amount: 20,
-    note: "some notes",
-    timestamp: "Today, 2:30 PM",
-  },
-  {
-    id: "002",
-    type: "out",
-    name: "user002",
-    amount: 56.01,
-    note: "asdwa asd wa sd asd wda sda wadafdgr ga ssad wa",
-    timestamp: "Yesterday, 10:15 AM",
-  },
-  {
-    id: "003",
-    type: "in",
-    name: "user003",
-    amount: 40,
-    note: "some notes",
-    timestamp: "Mar 25, 4:45 PM",
-  },
-  {
-    id: "004",
-    type: "in",
-    name: "user004",
-    amount: 59,
-    note: "some notes",
-    timestamp: "Mar 25, 4:45 PM",
-  },
-  {
-    id: "005",
-    type: "in",
-    name: "user005",
-    amount: 20,
-    note: "some notes",
-    timestamp: "Mar 25, 4:45 PM",
-  },
-  {
-    id: "006",
-    type: "out",
-    name: "user006",
-    amount: 20,
-    note: "some notes",
-    timestamp: "Mar 25, 4:45 PM",
-  },
-  {
-    id: "007",
-    type: "out",
-    name: "user007",
-    amount: 200.5,
-    note: "some notes",
-    timestamp: "Mar 25, 4:45 PM",
-  },
-  {
-    id: "008",
-    type: "in",
-    name: "user008",
-    amount: 15,
-    note: "some notes",
-    timestamp: "Mar 26, 4:45 PM",
-  },
-  {
-    id: "009",
-    type: "in",
-    name: "user001",
-    amount: 2.2,
-    note: "some notes",
-    timestamp: "Mar 20, 4:45 PM",
-  },
-];
-
 export default function HomeScreen() {
+  const [transactionHistory, setTransactionHistory] = useState<
+    TransactionHistory[]
+  >([]);
+
+  // Get transaction history from DB
+  const getTransactionHistory = useCallback(async () => {
+    const transactionHistoryService =
+      await TransactionHistoryService.getInstance();
+
+    const result = await transactionHistoryService.getAll();
+
+    setTransactionHistory(
+      result.map((r) => ({
+        id: r.id,
+        type: r.transferType,
+        name: r.userName,
+        amount: r.amount,
+        note: r.note,
+        timestamp: formatCustomRelativeDate(r.createdOn),
+      }))
+    );
+  }, []);
+
+  useFocusEffect(
+    // useCallback to prevent infinite loops
+    useCallback(() => {
+      getTransactionHistory();
+    }, [])
+  );
+
   return (
     <View style={styles.viewContainer}>
       <SectionLabel label="Transaction History" />
