d82131385652560d8d42e90c4ff7895c0594439a
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-30 12:39:14 +0800

    Support dark theme on <AppHeader />.

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,13 +1,21 @@
-import { Text, View } from "react-native-ui-lib";
-import { Avatar } from "./ui/Avatar";
 import { Colors } from "@/constants/Colors";
 import { StyleSheet } from "react-native";
+import { Text, View } from "react-native-ui-lib";
+import { Avatar } from "./ui/Avatar";
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import { useMemo } from "react";
 
 export function AppHeader() {
+  const colorScheme = useColorScheme();
+
+  const labelStyle = useMemo(() => {
+    return colorScheme === "light" ? styles.labelLight : styles.labelDark;
+  }, [colorScheme]);
+
   return (
     <View row centerV>
       <Avatar />
-      <Text text50BL style={styles.label}>
+      <Text text50BL style={labelStyle}>
         RightBank
       </Text>
     </View>
@@ -15,7 +23,10 @@ export function AppHeader() {
 }
 
 const styles = StyleSheet.create({
-  label: {
-    color: Colors.brand,
+  labelLight: {
+    color: Colors.primary.brand.light,
+  },
+  labelDark: {
+    color: Colors.primary.brand.dark,
   },
 });
