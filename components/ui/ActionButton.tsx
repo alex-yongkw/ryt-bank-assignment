53d3570fcc98b663b4894f05c7f636aab616d627
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-30 14:03:06 +0800

    Add dark theme supportr for <ActionButton />.

--- components/ui/ActionButton.tsx
+++ components/ui/ActionButton.tsx
@@ -1,5 +1,8 @@
 import { Colors } from "@/constants/Colors";
 import { Layout } from "@/constants/Layout";
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import { useMemo } from "react";
+import { StyleSheet } from "react-native";
 import { Button } from "react-native-ui-lib";
 
 type Props = {
@@ -8,13 +11,38 @@ type Props = {
 };
 
 export function ActionButton({ label, onPress }: Props) {
+  const colorScheme = useColorScheme();
+
+  const buttonColor = useMemo(() => {
+    return colorScheme === "light"
+      ? Colors.primary.brand.light
+      : Colors.primary.brand.dark;
+  }, [colorScheme]);
+
+  const labelStyle = useMemo(() => {
+    return colorScheme === "light" ? styles.labelLight : styles.labelDark;
+  }, [colorScheme]);
+
   return (
     <Button
       label={label}
       size={Button.sizes.large}
-      backgroundColor={Colors.brand}
+      labelStyle={labelStyle}
+      backgroundColor={buttonColor}
       borderRadius={Layout.actionButton.borderRadius}
       onPress={onPress}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  labelLight: {
+    fontSize: Layout.input.text,
+    color: "#ffffff",
+  },
+  labelDark: {
+    fontSize: Layout.input.text,
+    fontWeight: 700,
+    color: Colors.primary.brand.light,
+  },
+});
