03e0c36e5fd56f5e6643baf21729acd34c29f927
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-30 13:52:18 +0800

    Add dark theme for <ArrowDownIcon />.

--- components/ui/ArrowDownIcon.tsx
+++ components/ui/ArrowDownIcon.tsx
@@ -1,11 +1,21 @@
 import { Colors } from "@/constants/Colors";
 import { View } from "react-native-ui-lib";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import { useMemo } from "react";
 
 export function ArrowDownIcon() {
+  const colorScheme = useColorScheme();
+
+  const iconColor = useMemo(() => {
+    return colorScheme === "light"
+      ? Colors.primary.brand.light
+      : Colors.primary.brand.dark;
+  }, [colorScheme]);
+
   return (
     <View row centerH>
-      <FontAwesome5 name="arrow-down" size={28} color={Colors.brand} />
+      <FontAwesome5 name="arrow-down" size={28} color={iconColor} />
     </View>
   );
 }
