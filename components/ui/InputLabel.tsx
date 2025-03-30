869266dcc3be38702470b2a343bbcd33c9697b46
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-30 12:48:21 +0800

    Add dark theme support for <InputLabel />.

--- components/ui/InputLabel.tsx
+++ components/ui/InputLabel.tsx
@@ -1,4 +1,7 @@
+import { Colors } from "@/constants/Colors";
 import { Layout } from "@/constants/Layout";
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import { useMemo } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-ui-lib";
 
@@ -7,15 +10,26 @@ type Props = {
 };
 
 export function InputLabel({ label }: Props) {
+  const colorScheme = useColorScheme();
+
+  const labelStyle = useMemo(() => {
+    return colorScheme === "light" ? styles.labelLight : styles.labelDark;
+  }, [colorScheme]);
+
   return (
-    <Text text70BO style={styles.label}>
+    <Text text60M style={labelStyle}>
       {label}
     </Text>
   );
 }
 
 const styles = StyleSheet.create({
-  label: {
+  labelLight: {
+    color: Colors.text.light,
+    marginBottom: Layout.form.input.marginBottom,
+  },
+  labelDark: {
+    color: Colors.text.dark,
     marginBottom: Layout.form.input.marginBottom,
   },
 });
