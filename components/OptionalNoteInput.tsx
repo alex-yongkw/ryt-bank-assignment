5f6213b3722023f8b3105342eb905ab4cd978b41
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-30 13:13:26 +0800

    Add dark theme support for <OptionalNoteInput />.

--- components/OptionalNoteInput.tsx
+++ components/OptionalNoteInput.tsx
@@ -1,14 +1,28 @@
 import { TextField, View } from "react-native-ui-lib";
 import { InputLabel } from "./ui/InputLabel";
-
 import { Store } from "@/store";
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import { useMemo } from "react";
+import { Colors } from "@/constants/Colors";
+import { StyleSheet } from "react-native";
 
 export function OptionalNoteInput() {
+  const colorScheme = useColorScheme();
+
+  const placeholderTextColor = useMemo(() => {
+    return colorScheme === "light" ? Colors.text.light : Colors.text.dark;
+  }, [colorScheme]);
+
+  const textStyle = useMemo(() => {
+    return colorScheme === "light" ? styles.textLight : styles.textDark;
+  }, [colorScheme]);
+
   return (
     <View>
       <InputLabel label="Note (optional)" />
       <TextField
         placeholder="What's this for?"
+        placeholderTextColor={placeholderTextColor}
         multiline
         numberOfLines={6}
         maxLength={200}
@@ -16,7 +30,19 @@ export function OptionalNoteInput() {
         showClearButton
         onClear={() => Store.transfer.note.value.reset()}
         onChangeText={(text) => Store.transfer.note.value.set(text)}
+        style={textStyle}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textLight: {
+    fontSize: 16,
+    color: Colors.text.light,
+  },
+  textDark: {
+    fontSize: 16,
+    color: Colors.text.dark,
+  },
+});
