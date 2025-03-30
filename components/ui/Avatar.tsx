8b214cf40fef52965e3dc20218d5504dba8d9b49
alex-y0ng <blazespiritzzzz@gmail.com>
2025-03-30 12:39:50 +0800

    Support dark theme for <Avatar /> component.

--- components/ui/Avatar.tsx
+++ components/ui/Avatar.tsx
@@ -1,14 +1,29 @@
 import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import { useMemo } from "react";
 import { StyleSheet } from "react-native";
 import { Card } from "react-native-ui-lib";
 
 const DIMENSION = 40;
 
 export function Avatar() {
+  const colorScheme = useColorScheme();
+
+  const cardStyle = useMemo(() => {
+    return colorScheme === "light" ? styles.cardLight : styles.cardDark;
+  }, [colorScheme]);
+
   return (
-    <Card width={DIMENSION} height={DIMENSION} style={styles.card}>
+    <Card width={DIMENSION} height={DIMENSION} style={cardStyle}>
       <Card.Section
-        content={[{ text: "RB", text60: true, white: true }]}
+        content={[
+          {
+            text: "RB",
+            text60: true,
+            color:
+              colorScheme === "light" ? "#ffffff" : Colors.primary.brand.light,
+          },
+        ]}
         contentStyle={styles.cardContent}
       />
     </Card>
@@ -16,11 +31,16 @@ export function Avatar() {
 }
 
 const styles = StyleSheet.create({
-  card: {
-    backgroundColor: Colors.brand,
+  cardLight: {
+    backgroundColor: Colors.primary.brand.light,
+    marginRight: 10,
+  },
+  cardDark: {
+    backgroundColor: Colors.primary.brand.dark,
     marginRight: 10,
   },
   cardContent: {
+    color: "purple",
     height: DIMENSION,
     alignSelf: "center",
     justifyContent: "center",
